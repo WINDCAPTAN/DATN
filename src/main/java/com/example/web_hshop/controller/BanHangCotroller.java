1db4b620c5d48c33f877cdad10d3eb09f19286e2
WINDCAPTAN <phongnhp23@gmail.com>
2024-07-09 17:43:07 +0700

    update bán hàng

--- src/main/java/com/example/web_hshop/controller/BanHangCotroller.java
+++ src/main/java/com/example/web_hshop/controller/BanHangCotroller.java
@@ -1,17 +1,24 @@
 package com.example.web_hshop.controller;
 
+import com.example.web_hshop.entity.ThuongHieu;
+import com.example.web_hshop.repository.ChatLieuRepository;
+import com.example.web_hshop.repository.KichThuocRepository;
+import com.example.web_hshop.repository.MauSacRepository;
+import com.example.web_hshop.repository.SanPhamChiTietRepository;
+import com.example.web_hshop.repository.SanPhamRepository;
+import com.example.web_hshop.repository.ThuongHieuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BanHangCotroller {
-// test
 
     @GetMapping("/admin")
-    public String hienThi(){
+    public String hienThi() {
         return "index";
     }
-
 
 
 }
--- src/main/java/com/example/web_hshop/controller/BanTaiQuayController.java
+++ src/main/java/com/example/web_hshop/controller/BanTaiQuayController.java
@@ -1,13 +1,46 @@
 package com.example.web_hshop.controller;
 
+import com.example.web_hshop.repository.ChatLieuRepository;
+import com.example.web_hshop.repository.KichThuocRepository;
+import com.example.web_hshop.repository.MauSacRepository;
+import com.example.web_hshop.repository.SanPhamChiTietRepository;
+import com.example.web_hshop.repository.SanPhamRepository;
+import com.example.web_hshop.repository.ThuongHieuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BanTaiQuayController {
 
     @GetMapping("/ban-hang")
-    public String banHang(){
+    public String banHang(Model model){
+        model.addAttribute("th",thuongHieurepo.findAll());
+        model.addAttribute("sp",sanPhamrepo.findAll());
+        model.addAttribute("spct", sanPhamCTrepo.findAll());
+        model.addAttribute("ms", mauSacRepository.findAll());
+        model.addAttribute("kv", kichThuocrepo.findAll());
+        model.addAttribute("cl", chatLieurepo.findAll());
         return "category/bantaiquay";
     }
+    @Autowired
+    ChatLieuRepository chatLieurepo;
+
+    @Autowired
+    KichThuocRepository kichThuocrepo;
+
+    @Autowired
+    MauSacRepository mauSacRepository;
+
+    @Autowired
+    SanPhamChiTietRepository sanPhamCTrepo;
+
+    @Autowired
+    SanPhamRepository sanPhamrepo;
+
+    @Autowired
+    ThuongHieuRepository thuongHieurepo;
+
+
 }
